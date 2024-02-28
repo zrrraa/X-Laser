88711e2a19815c900ff888fed868fa4d05d18fa6
zrrraa <3210104049@zju.edu.cn>
2024-02-28 17:14:13 +0800

    24.2.28 drawnow模式调通

---
+++ MyUI/SqureLine/SquareLine_Project_events.py
@@ -0,0 +1,60 @@
+
+def sdcardfile_1(event_struct):
+    return
+
+
+def draw_sdcardfile_2(event_struct):
+    return
+
+
+def draw_sdcardfile_1(event_struct):
+    return
+
+
+def draw_sdcardfile_3(event_struct):
+    return
+
+
+def draw_sdcardfile_4(event_struct):
+    return
+
+
+def Roller_Draw(event_struct):
+    return
+
+
+def GO_NEXTILDA(event_struct):
+    return
+
+
+def GO_PREVIOUSILDA(event_struct):
+    return
+
+
+def Go_ReFresh(event_struct):
+    return
+
+
+def GO_AutoPlay(event_struct):
+    return
+
+
+def sc3tosc2(event_struct):
+    return
+
+
+def Go_Pause(event_struct):
+    return
+
+
+def GO_Clear(event_struct):
+    return
+
+
+def GO_DrawNow(event_struct):
+    return
+
+
+def sc4tosc2(event_struct):
+    return
+
